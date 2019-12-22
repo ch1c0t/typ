fb52b0a7444b46206a0750001f4d975c3db21f54
Anatoly Chernow <chertoly@gmail.com>
2019-12-22 13:17:19 +0200

    Delete Mutant
    Because using refinements results in the Neutral failure.

--- spec/helper.rb
+++ spec/helper.rb
@@ -3,15 +3,3 @@ require 'typ'
 require 'rspec/power_assert'
 RSpec::PowerAssert.example_assertion_alias :assert
 RSpec::PowerAssert.example_group_assertion_alias :assert
-
-require_relative 'mutant'
-
-RSpec.configure do |config|
-  unless ENV['PRY']
-    require 'timeout'
-
-    config.around :each do |example|
-      Timeout.timeout 5, &example
-    end
-  end
-end
--- spec/mutant.rb
+++
@@ -1,9 +0,0 @@
-if defined? Mutant::CLI
-  module Mutant
-    class Selector::Expression
-      def call _subject
-        integration.all_tests
-      end
-    end
-  end
-end
