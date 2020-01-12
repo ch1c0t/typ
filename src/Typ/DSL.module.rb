e1e252d357647f2b40228a22fabb55a2893cd1aa
Anatoly Chernow <chertoly@gmail.com>
2020-01-12 09:17:06 +0200

    Refactor DSL#is

--- src/Typ/DSL.module.rb
+++ src/Typ/DSL.module.rb
@@ -5,13 +5,18 @@ end
 def is type
   case type
   when Symbol
-    gates << Is::Symbol.new(type)
+    symbol = type
+    test = -> it { it.send symbol }
+    gates << make_gate_with(test)
   when Array
-    unless type[0].is_a?(Symbol) || type[1].is_a?(Symbol)
+    array = type
+
+    unless array[0].is_a?(Symbol) || array[1].is_a?(Symbol)
       fail CannotCreateGate.new(__method__, type)
     end
 
-    gates << Is::Array.new(type)
+    test = array.to_proc
+    gates << make_gate_with(test)
   when Class
     if type.include? Typ
       gates << type
@@ -22,3 +27,16 @@ def is type
     fail CannotCreateGate.new(__method__, type)
   end
 end
+
+private
+  def make_gate_with test
+    check = Module.new do
+      define_method :check do
+        test[it]
+      end
+    end
+
+    gate = Gate.new
+    gate.include check
+    gate
+  end
